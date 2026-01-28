fe818124542c582dd505f990cd8c05f996795316
Juozas Skarbalius <juozas2003@gmail.com>
2026-01-28 13:14:11 +0100

    refactor: add asymmetric LCM test (#3)

--- src/main/java/com/skarbalius/task/PUMHandler.java
+++ src/main/java/com/skarbalius/task/PUMHandler.java
@@ -7,29 +7,29 @@ public class PUMHandler extends TaskHandler {
     @Override
     public void handle(TaskContext context) {
 
-        context.pum = new java.util.Vector<>(context.NUMPOINTS);
+        context.pum = new java.util.Vector<>(15);
 
         for (int i = 0; i < 15; i++) {
-            Vector<Boolean> row = new Vector<>(context.NUMPOINTS);
+            Vector<Boolean> row = new Vector<>(15);
 
             for (int j = 0; j < 15; j++) {
                 BooleanOperator op = context.lcm.get(i).get(j);
                 boolean val;
 
                 if (op == BooleanOperator.NOTUSED) {
-                    val = true; // NOTUSED 
+                    val = true;
                 } else if (op == BooleanOperator.ANDD) {
-                    val = context.cmv.get(i) && context.cmv.get(j); // ANDD
-                } else { 
-                    val = context.cmv.get(i) || context.cmv.get(j); // ORR
+                    val = context.cmv.get(i) && context.cmv.get(j);
+                } else {
+                    val = context.cmv.get(i) || context.cmv.get(j);
                 }
                 row.add(val);
             }
             context.pum.add(row);
         }
 
-        System.out.println("PUMHandler");
-        // super.setNextHandler(new FUVHandler());
-        // super.next(context);
+        System.out.println("PUMHandler finished");
+        super.setNextHandler(new FUVHandler());
+        super.next(context);
     }
 }
