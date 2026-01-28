cc61ba587d1a7a270b7531cb2d880de1c08b2c9e
Bahar Kimanos <bahar.kimanos@gmail.com>
2026-01-29 00:51:15 +0100

    fix: edits to FUVHandler

--- src/main/java/com/skarbalius/task/FUVHandler.java
+++ src/main/java/com/skarbalius/task/FUVHandler.java
@@ -1,17 +1,10 @@
 package com.skarbalius.task;
 
-import com.skarbalius.input.BooleanOperator;
-import java.util.Vector;
-
 public class FUVHandler extends TaskHandler{
-    private static final int SIZE = 15;
 
     @Override
     public void handle(TaskContext context) {
         context.fuv = new java.util.Vector<>(context.NUMPOINTS);
-        super.setNextHandler(new FinalDecisionHandler());
-        super.next(context);
-
         for (int i = 0; i < 15; i++) {
             if (context.puv.get(i) == false) {
                 context.fuv.add(true);
